9edc88b1ae14364cca7995dee40c6547efa3c1e0
Anand S Pai <81501797+AnandP201@users.noreply.github.com>
2023-06-04 20:59:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,14 +16,6 @@ const taskRouter=require('./src/routes/tasks')
 
 app.use('/tasks',taskRouter);
 
-if(process.env.NODE_ENV=="production"){
-  app.use(express.static('./build'))
-  const path=require('path')
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'build','index.html'))
-  })
-}
-
 app.listen(PORT, () => { 
   console.log(`Server listening on ${PORT}`);
 });
